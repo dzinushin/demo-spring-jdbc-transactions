5672c00f1bb629bdb33e77384c6b79ec9c933e6e
Dmitry Zinushin <zinushin@gmail.com>
2012-04-25 19:53:34 +0400

    big deal

--- src/main/java/ru/dzinushin/demos/spring/transactions/Main.java
+++ src/main/java/ru/dzinushin/demos/spring/transactions/Main.java
@@ -1,10 +1,12 @@
 package ru.dzinushin.demos.spring.transactions;
 
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Main {
+import ru.dzinushin.demos.spring.transactions.service.StartService;
 
+public class Main {
 	/**
 	 * @param args
 	 */
@@ -13,6 +15,11 @@ public class Main {
 		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
 
+		StartService srv = (StartService) ctx.getBean("StartService");
+		
+		//srv.testApacheDBCP();
+		srv.insertData();
+		srv.insertData2();
 	}
 
 }
